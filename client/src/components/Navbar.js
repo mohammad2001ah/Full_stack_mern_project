4fe0f61cb8ee611cfb681f4053544397881f0df3
mohammad2001ah <mah.2001.ah@gmail.com>
2025-10-07 02:14:35 +0300

    frontend navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,67 @@
-import logo from"../images/logo.png";
 import { NavLink } from "react-router-dom";
+//fontawesome icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+// css file
+import "./navbar.css";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center py-5 font-medium ">
-      <img src={logo} className="w-41" alt="Logo" />
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            My STORE
+          </NavLink>
 
-      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <NavLink className="flex flex-col items-center gap-1" >
-          <p>Home</p>
-          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700" />
-
-        </NavLink>
-      </ul>
+          <div
+            className="collapse navbar-collapse justify-content-center "
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" to="/">
+                  HOME
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  aria-current="page"
+                  to="/collection"
+                >
+                  COLLECTION
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" to="/about">
+                  ABOUT
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" to="/contact">
+                  CONTACT
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="d-flex search" style={{ cursor: "pointer" }}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </div>
+        </div>
+      </nav>
     </div>
   );
-};
+}
