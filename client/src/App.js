c6c776589ad59fa7d43eef9c5de00ffcb85e6ba0
mohammad2001ah <mah.2001.ah@gmail.com>
2025-10-13 12:53:24 +0300

    5

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,10 @@ import { BrowserRouter,Router, Route, Routes,useLocation } from 'react-router-do
 
 // Import Pages
 import About from './components/About';
-import Cart from './pages/Cart';
 import Collection from './components/Collection';
 import Contact from './components/Contact';
 import Home from './components/Home';
 import Login from './components/Login';
-import Orders from './pages/Orders';
 import Footer from './components/Footer';
 import Signup from './components/Signup';
 import AdminDashboard from './components/AdminDashboard';
@@ -26,11 +24,9 @@ function App() {
         <Route path='/collection' element={<Collection/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
-        <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/admin' element={<AdminDashboard/>}/>
-        <Route path='/orders' element={<Orders/>}/>
       </Routes>
       {/* <Footer/> */}
       {location.pathname !== "/admin" && <Footer />}
